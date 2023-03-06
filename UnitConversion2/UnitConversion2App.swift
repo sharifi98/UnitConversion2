3fd3126a995b4584aa8f29a7d6de39f629e56acb
Hossein Sharifi <47387238+sharifi98@users.noreply.github.com>
2023-03-06 12:27:24 +0100

    Initial Commit

---
+++ UnitConversion2/UnitConversion2App.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversion2App.swift
+//  UnitConversion2
+//
+//  Created by Hossein Sharifi on 06/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversion2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
